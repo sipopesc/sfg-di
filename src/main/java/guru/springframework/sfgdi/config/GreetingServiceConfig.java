e1f3790cc7a7d14ba7faf93c573df995f0185846
Silviu Constantin Popescu <constantinsilviupopescu@gmail.com>
2022-07-04 10:52:10 +0300

    Constructor Configuration properties binding

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -9,11 +9,13 @@ import guru.springframework.sfgdi.services.ConstructorInjectedGreetingService;
 import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
 import guru.springframework.sfgdi.services.I18nSpanishGreetingService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
+@EnableConfigurationProperties(SfgConstructorConfig.class)
 @PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
